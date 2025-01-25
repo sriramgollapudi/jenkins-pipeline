4d725adfacbc0b1b6d3fd4508843b38679685424
Sri <gollapudiram@gmail.com>
2025-01-25 15:30:53 +0000

    changing bucket name

--- 1-terraform-jenkins-server/backend.tf
+++ 1-terraform-jenkins-server/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2024-v2"
+    bucket = "jenkins-app-kub-2024-v2-1"
     region = "eu-west-3"
     key = "jenkins-server/terraform.tfstate"
   }
--- 2-terraform-eks-deployment/backend.tf
+++ 2-terraform-eks-deployment/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-app-kub-2024-v2"
+    bucket = "jenkins-app-kub-2024-v2-1"
     region = "eu-west-3"
     key = "eks/terraform.tfstate"
   }
